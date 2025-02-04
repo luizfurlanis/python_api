d5114d384667397ab8f1ea14571f32c861775a82
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-02-04 15:20:07 -0300

    [feat] ui for methods on the api

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, render_template
 import mysql.connector
 from flask_cors import CORS
+
 
 mydb = mysql.connector.connect(
     host='localhost',
@@ -8,13 +9,13 @@ mydb = mysql.connector.connect(
     password='ROLONnl3@',
     database='cadastros'
 )
-
 app = Flask(__name__)
 CORS(app)
 
+
+# GET method for the website
 @app.route('/quadros', methods = ['GET'])
 def get_products():
-
     mycursor = mydb.cursor()
     mycursor.execute('SELECT * FROM cadastros.produtos')
     produtos = mycursor.fetchall()
@@ -36,10 +37,29 @@ def get_products():
 
     return jsonify(quadro)
 
+
+# GET method for the user UI
+@app.route('/add', methods=['GET'])
+def add_product_form():
+    return render_template('add_products.html')
+
+
+# POST method for adding new product
 @app.route('/quadros', methods=['POST'])
 def create_quadro():
-    newquadro = request.json
-
+    if request.is_json:
+        newquadro = request.json
+    else:
+        newquadro = {
+            'titulo' : request.form['titulo'],
+            'preco' : float(request.form['preco']),
+            'categoria' : request.form['categoria'],
+            'imagem' : request.form['imagem'],
+            'descricao' : request.form['descricao'],
+            'cor' : request.form['cor'],
+            'tamanho' : request.form['tamanho']
+        }    
+    
     sql = "INSERT INTO cadastros.produtos (titulo, preco, categoria, imagem, descricao, cor, tamanho) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     valores = (newquadro['titulo'], newquadro['preco'], newquadro['categoria'], newquadro['imagem'], newquadro['descricao'], newquadro['cor'], newquadro['tamanho'])
 
@@ -49,5 +69,5 @@ def create_quadro():
 
     return jsonify(mensagem = 'Produto cadastrado!', quadro = newquadro)
 
-
-app.run()
+# Run the API app
+app.run(debug=True)
