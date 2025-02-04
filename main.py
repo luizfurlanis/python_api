2a8725a925a0c9335df4326e9d849e6871076d8d
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-02-04 15:27:04 -0300

    [refactor] protect credentials

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 
 
 mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='ROLONnl3@',
-    database='cadastros'
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME')
 )
 app = Flask(__name__)
 CORS(app)
